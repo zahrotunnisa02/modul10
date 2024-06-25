0aece6c1b38e1316117b6b9e8aea739d40711b10
zahrotunnisa02 <164749280+zahrotunnisa02@users.noreply.github.com>
2024-06-26 01:15:13 +0700

    Add files via upload

---
+++ storage/framework/views/0925a5c447417b739d04a3a1b172497e.php
@@ -0,0 +1,48 @@
+<?php $__env->startSection('content'); ?>
+    <div class="container mt-4">
+        <div class="row mb-0">
+            <div class="col-lg-9 col-xl-6">
+                <h4 class="mb-3"><?php echo e($pageTitle); ?></h4>
+            </div>
+            <div class="col-lg-3 col-xl-6">
+                <ul class="list-inline mb-0 float-end">
+                    <li class="list-inline-item">
+                        <a href="<?php echo e(route('employees.exportExcel')); ?>" class="btn btn-outline-success">
+                            <i class="bi bi-download me-1"></i> to Excel
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="<?php echo e(route('employees.exportPdf')); ?>" class="btn btn-outline-danger">
+                            <i class="bi bi-download me-1"></i> to PDF
+                        </a>
+                    </li>
+                    <li class="list-inline-item">|</li>
+                    <li class="list-inline-item">
+                        <a href="<?php echo e(route('employees.create')); ?>" class="btn btn-primary">
+                            <i class="bi bi-plus-circle me-1"></i> Create Employee
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="table-responsive border p-3 rounded-3 mb-5">
+            <table class="table table-bordered table-hover table-striped mb-0 bg-white datatable" id="employeeTable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>No.</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Position</th>
+                        <th></th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\User\Downloads\modul10-master\resources\views/employee/index.blade.php ENDPATH**/ ?>
